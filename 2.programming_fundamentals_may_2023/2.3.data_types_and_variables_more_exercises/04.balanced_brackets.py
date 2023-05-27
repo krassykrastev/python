684c683a077ed2d1d1bc1a107619e8cf4b458a3d
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-05-27 08:36:27 +0200

    Update 04.balanced_brackets.py

--- 2.programming_fundamentals_may_2023/2.3.data_types_and_variables_more_exercises/04.balanced_brackets.py
+++ 2.programming_fundamentals_may_2023/2.3.data_types_and_variables_more_exercises/04.balanced_brackets.py
@@ -20,3 +20,25 @@ if opening_bracket and closing_bracket and balanced:
     print('BALANCED')
 else:
     print('UNBALANCED')
+
+# another solution from raya
+# n = int(input())
+# example = ""
+# not_balanced = False
+#
+# for i in range(1, n + 1):
+#     text = input()
+#     if text != "(" and text != ")":
+#         continue
+#     if text == "(" and example == "":
+#         example += "("
+#     elif text == ")" and example == "(":
+#         example = ""
+#     else:
+#         not_balanced = True
+#         break
+#
+# if not not_balanced:
+#     print("BALANCED")
+# else:
+#     print("UNBALANCED")
