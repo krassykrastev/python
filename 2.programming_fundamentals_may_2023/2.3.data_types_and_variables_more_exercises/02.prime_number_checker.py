f8b7c02fbd9c308a6555ba63f111d418599b9092
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-05-27 07:58:40 +0200

    Update 02.prime_number_checker.py

--- 2.programming_fundamentals_may_2023/2.3.data_types_and_variables_more_exercises/02.prime_number_checker.py
+++ 2.programming_fundamentals_may_2023/2.3.data_types_and_variables_more_exercises/02.prime_number_checker.py
@@ -1,11 +1,21 @@
-is_prime = False
+is_prime = True
 number = int(input())
+
 if number > 2:
     for i in range(2, number):
         if number % i == 0:
-            is_prime = True
+            is_prime = False
             break
-if not is_prime:
-    print('True')
-else:
-    print('False')
+
+print(is_prime)
+
+# another solution from Stefi: Тони Троев ни каза за това математически доказано правило.
+# При много големите числа цикъла ненужно ще върти. Ето защо може да върти само до корен квадратен от входа.
+# from math import sqrt
+# user_input = int(input())
+# sqrt_to_input = int(sqrt(user_input))
+# is_prime = True
+# for i in range(2, sqrt_to_input + 1):
+#     if user_input % i == 0:
+#         is_prime = False
+# print(is_prime)
