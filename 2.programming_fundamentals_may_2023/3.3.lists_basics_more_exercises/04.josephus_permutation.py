96e186cc5a14d44c84544f630534c4f85b42c71c
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-05-28 14:42:30 +0200

    Create 04.josephus_permutation.py

---
+++ 2.programming_fundamentals_may_2023/3.3.lists_basics_more_exercises/04.josephus_permutation.py
@@ -0,0 +1,15 @@
+# solution from ceo as I was not able to find solution myself
+main_list = input().split()
+skip_number_k = int(input())
+
+final_numbers = []
+pos = skip_number_k - 1
+index = 0
+len_list = (len(main_list))
+
+while len_list > 0:
+    index = (pos + index) % len(main_list)
+    final_numbers.append(main_list.pop(index))
+    len_list -= 1
+
+print(f"[{','.join(final_numbers)}]")
