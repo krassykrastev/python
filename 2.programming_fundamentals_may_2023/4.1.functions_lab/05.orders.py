bb7089aa74b587384c09f9727080d73f24b4fe46
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-06-02 23:35:25 +0200

    Update 05.orders.py

--- 2.programming_fundamentals_may_2023/4.1.functions_lab/05.orders.py
+++ 2.programming_fundamentals_may_2023/4.1.functions_lab/05.orders.py
@@ -1,14 +1,14 @@
-def calculate_total_order_value():
-    if product == 'coffee':
+def calculate_total_order_value(some_product):
+    if some_product == 'coffee':
         return 1.50 * quantity
-    elif product == 'water':
+    elif some_product == 'water':
         return 1.00 * quantity
-    elif product == 'coke':
+    elif some_product == 'coke':
         return 1.40 * quantity
-    elif product == 'snacks':
+    elif some_product == 'snacks':
         return 2.00 * quantity
 
 product = input()
 quantity = int(input())
 
-print(f'{calculate_total_order_value():.2f}')
+print(f'{calculate_total_order_value(product):.2f}')
