b847859e49692ebcdfe7d45d7329ab3b76fa1daa
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-06-05 14:42:21 +0200

    Update 10.example_currency_convertor.py

--- 2.programming_fundamentals_may_2023/4.1.functions_lab/10.example_currency_convertor.py
+++ 2.programming_fundamentals_may_2023/4.1.functions_lab/10.example_currency_convertor.py
@@ -1,8 +1,7 @@
 from forex_python.converter import CurrencyRates
 
 def get_exchange_rate(base_currency, target_currency):
-    c = CurrencyRates()
-    exchange_rate = c.get_rate(base_currency, target_currency)
+    exchange_rate = CurrencyRates().get_rate(base_currency, target_currency)
     return exchange_rate
 
 def convert_currency(base_currency, target_currency, amount):
