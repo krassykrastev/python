118ea34a06f9cc3389c83b9617e4d069a1890783
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-06-11 19:23:08 +0200

    Update 03.word_reverse.py

--- 2.programming_fundamentals_may_2023/1.1.basic_syntax_conditional_statements_and_loops_lab/03.word_reverse.py
+++ 2.programming_fundamentals_may_2023/1.1.basic_syntax_conditional_statements_and_loops_lab/03.word_reverse.py
@@ -1,3 +1,11 @@
+# Write a program that receives a single word, reverses it, and prints it.
+#
+# Input1: Python
+# Output1: nohtyP
+#
+# Input2: banana
+# Output2: ananab
+
 word = input()
 
 for i in range(len(word) - 1, -1, -1):
