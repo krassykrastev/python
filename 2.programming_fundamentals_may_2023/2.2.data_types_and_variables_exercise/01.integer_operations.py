8976309d6bc6b53efcb5975f71fe374b95b9b614
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-06-11 19:25:04 +0200

    Update 01.integer_operations.py

--- 2.programming_fundamentals_may_2023/2.2.data_types_and_variables_exercise/01.integer_operations.py
+++ 2.programming_fundamentals_may_2023/2.2.data_types_and_variables_exercise/01.integer_operations.py
@@ -1,3 +1,22 @@
+# Write a program that reads four integer numbers.
+# It should add the first to the second number, integer divide the sum by the third number, and multiply the result by the fourth number.
+# Print the final result.
+#
+# Input1:
+# 10
+# 20
+# 3
+# 3
+# Output1: 30
+#
+# Input2:
+# 15
+# 14
+# 2
+# 3
+#
+# Output2: 42
+
 first_number = int(input())
 second_number = int(input())
 third_number = int(input())
