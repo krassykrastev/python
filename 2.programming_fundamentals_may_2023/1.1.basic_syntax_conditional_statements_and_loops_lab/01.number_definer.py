b6aa1987a753c56de90c72e95ba6f072109fa1aa
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-06-11 19:22:38 +0200

    Update 01.number_definer.py

--- 2.programming_fundamentals_may_2023/1.1.basic_syntax_conditional_statements_and_loops_lab/01.number_definer.py
+++ 2.programming_fundamentals_may_2023/1.1.basic_syntax_conditional_statements_and_loops_lab/01.number_definer.py
@@ -1,3 +1,26 @@
+# Write a program that reads a floating-point number and:
+# - prints "zero" if the number is zero
+# - prints "positive" or "negative"
+# - adds "small" if the absolute value of the number is less than 1 and it is not 0, or "large" if it exceeds 1 000 000
+#
+# Input1: 25
+# Output1: positive
+#
+# Input2: 0.7
+# Output2: small positive
+#
+# Input3: 435247392.921
+# Output3: large positive
+#
+# Input4: -0.005
+# Output4: small negative
+#
+# Input5: -103.21
+# Output5: negative
+#
+# Input6: -358583355123.001
+# Output6: large negative
+
 num = float(input())
 
 if num == 0:
