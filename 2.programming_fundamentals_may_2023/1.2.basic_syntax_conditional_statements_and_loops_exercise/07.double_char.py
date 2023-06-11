eea78a30186dedca8f5ac2b7843ac4a9cbf12527
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-06-11 19:24:20 +0200

    Update 07.double_char.py

--- 2.programming_fundamentals_may_2023/1.2.basic_syntax_conditional_statements_and_loops_exercise/07.double_char.py
+++ 2.programming_fundamentals_may_2023/1.2.basic_syntax_conditional_statements_and_loops_exercise/07.double_char.py
@@ -1,3 +1,26 @@
+# You will be given strings until you receive the command "End".
+# For each string given, you should print a string in which each character (case-sensitive) is repeated twice.
+# Note that if you receive the string "SoftUni", you should NOT print it!
+#
+# Input1:
+# Hello World
+# Repeat
+# End
+#
+# Output1:
+# HHeelllloo WWoorrlldd
+# RReeppeeaatt
+#
+# Input2:
+# 1234!
+# SoftUni
+# softuni
+# End
+#
+# Output2:
+# 11223344!!
+# ssooffttuunnii
+
 current_string = input()
 
 while current_string != 'End':
