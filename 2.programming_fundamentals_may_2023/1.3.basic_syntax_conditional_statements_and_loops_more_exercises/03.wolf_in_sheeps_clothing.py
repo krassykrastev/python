72c5a6183fb5ba5112632bc1a7fc855b56c326a4
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-06-11 19:24:42 +0200

    Update 03.wolf_in_sheeps_clothing.py

--- 2.programming_fundamentals_may_2023/1.3.basic_syntax_conditional_statements_and_loops_more_exercises/03.wolf_in_sheeps_clothing.py
+++ 2.programming_fundamentals_may_2023/1.3.basic_syntax_conditional_statements_and_loops_more_exercises/03.wolf_in_sheeps_clothing.py
@@ -1,3 +1,19 @@
+# Wolves have been reintroduced to Great Britain. You are a sheep farmer and are now plagued by wolves who pretend to be sheep.
+# Fortunately, you are good at spotting them.
+# Warn the sheep in front of the wolf that it is about to be eaten.
+# Remember that you are standing at the front of the queue, which is at the end of the list:
+# [sheep, sheep, wolf, sheep, sheep] (YOU ARE HERE AT THE FRONT OF THE QUEUE)
+# 4 3 2 1
+# If the wolf is the closest animal to you, print "Please go away and stop eating my sheep".
+# Otherwise, return "Oi! Sheep number N! You are about to be eaten by a wolf!" where N is the sheep's position in the queue.
+# Note: there will always be exactly one wolf on the list.
+#
+# Input1: sheep, sheep, wolf
+# Output1: Please go away and stop eating my sheep
+#
+# Input2: wolf, sheep, sheep, sheep, sheep, sheep
+# Output2: Oi! Sheep number 5! You are about to be eaten by a wolf!
+
 sheep = 0
 
 text = input()
