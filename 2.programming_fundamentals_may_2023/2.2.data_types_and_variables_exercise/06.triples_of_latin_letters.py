8c968bb5b053420ca3d46095b31f25ac5281006d
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-06-11 19:25:19 +0200

    Update 06.triples_of_latin_letters.py

--- 2.programming_fundamentals_may_2023/2.2.data_types_and_variables_exercise/06.triples_of_latin_letters.py
+++ 2.programming_fundamentals_may_2023/2.2.data_types_and_variables_exercise/06.triples_of_latin_letters.py
@@ -1,3 +1,48 @@
+# Write a program to read an integer N and print all triples of the first N small Latin letters, ordered alphabetically
+#
+# Input1: 3
+#
+# Output1:
+# aaa
+# aab
+# aac
+# aba
+# abb
+# abc
+# aca
+# acb
+# acc
+# baa
+# bab
+# bac
+# bba
+# bbb
+# bbc
+# bca
+# bcb
+# bcc
+# caa
+# cab
+# cac
+# cba
+# cbb
+# cbc
+# cca
+# ccb
+# ccc
+#
+# Input2: 2
+#
+# Output2:
+# aaa
+# aab
+# aba
+# abb
+# baa
+# bab
+# bba
+# bbb
+
 number_of_symbols = int(input())
 for first_symbol in range(number_of_symbols):
     for second_symbol in range(number_of_symbols):
