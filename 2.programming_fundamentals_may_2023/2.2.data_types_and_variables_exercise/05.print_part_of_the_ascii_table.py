e625f224949db12d8b2d3eff5b76a44abd681756
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-06-11 19:25:16 +0200

    Update 05.print_part_of_the_ascii_table.py

--- 2.programming_fundamentals_may_2023/2.2.data_types_and_variables_exercise/05.print_part_of_the_ascii_table.py
+++ 2.programming_fundamentals_may_2023/2.2.data_types_and_variables_exercise/05.print_part_of_the_ascii_table.py
@@ -1,3 +1,25 @@
+# Write a program that prints part of the ASCII table characters on the console, separated by a single space.
+# On the first line of input, you will receive the char index you should start with.
+# On the second line - the index of the last character you should print.
+#
+# Input1:
+# 60
+# 65
+#
+# Output1: < = > ? @ A
+#
+# Input2:
+# 69
+# 79
+#
+# Output2: E F G H I J K L M N O
+#
+# Input3:
+# 97
+# 104
+#
+# Output3: a b c d e f g h
+
 start_index = int(input())
 final_index = int(input())
 for character in range(start_index, final_index + 1):
