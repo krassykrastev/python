33b5bc3db3ed28005b8c10f5c8ffefd07791be38
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-06-11 19:25:32 +0200

    Update 01.exchange_integers.py

--- 2.programming_fundamentals_may_2023/2.3.data_types_and_variables_more_exercises/01.exchange_integers.py
+++ 2.programming_fundamentals_may_2023/2.3.data_types_and_variables_more_exercises/01.exchange_integers.py
@@ -1,3 +1,29 @@
+# Read two integer numbers and, after that, exchange their values. Print the variable values before and after the exchange
+#
+# Input1:
+# 5
+# 10
+#
+# Output1:
+# Before:
+# a = 5
+# b = 10
+# After:
+# a = 10
+# b = 5
+#
+# Input2:
+# 10
+# 20
+#
+# Output2:
+# Before:
+# a = 10
+# b = 20
+# After:
+# a = 20
+# b = 10
+
 number_a = int(input())
 number_b = int(input())
 
