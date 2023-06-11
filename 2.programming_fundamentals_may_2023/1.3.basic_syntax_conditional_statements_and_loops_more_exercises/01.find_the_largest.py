97350f126374f764c47061edfaf9c7e6f7be028b
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-06-11 19:24:36 +0200

    Update 01.find_the_largest.py

--- 2.programming_fundamentals_may_2023/1.3.basic_syntax_conditional_statements_and_loops_more_exercises/01.find_the_largest.py
+++ 2.programming_fundamentals_may_2023/1.3.basic_syntax_conditional_statements_and_loops_more_exercises/01.find_the_largest.py
@@ -1,3 +1,11 @@
+# You will be given a number. Print the largest number that can be formed from the digits of the given number.
+#
+# Input1: 213
+# Output1: 321
+#
+# Input2: 7389
+# Output2: 9873
+
 num = input()
 current_number = ''
 
