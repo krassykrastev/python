55e8a4cd2ff96cf7bb5fc057ab2b8968d4b7cb7d
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-06-11 19:24:50 +0200

    Update 02.convert_meters_to_kilometers.py

--- 2.programming_fundamentals_may_2023/2.1.data_types_and_variables_lab/02.convert_meters_to_kilometers.py
+++ 2.programming_fundamentals_may_2023/2.1.data_types_and_variables_lab/02.convert_meters_to_kilometers.py
@@ -1,3 +1,12 @@
+# You will be given an integer that represents a distance in meters.
+# Write a program that converts meters to kilometers formatted to the second decimal point.
+#
+# Input1: 1852
+# Output1: 1.85
+#
+# Input2: 798
+# Output2: 0.80
+
 meter = int(input())
 kilometers = meter / 1000
 
