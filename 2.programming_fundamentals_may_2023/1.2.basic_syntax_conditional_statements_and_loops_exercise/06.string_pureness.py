6bdb340a1976ea7f1c20db79dcc7f87875838721
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-06-11 19:24:17 +0200

    Update 06.string_pureness.py

--- 2.programming_fundamentals_may_2023/1.2.basic_syntax_conditional_statements_and_loops_exercise/06.string_pureness.py
+++ 2.programming_fundamentals_may_2023/1.2.basic_syntax_conditional_statements_and_loops_exercise/06.string_pureness.py
@@ -1,3 +1,29 @@
+# You will be given number n. After that, you'll receive different strings n times.
+# Your task is to check if the given strings are pure, meaning that they do NOT consist of any of the characters:
+# comma ",", period ".", or underscore "_":
+# • If a string is pure, print "{string} is pure."
+# • Otherwise, print "{string} is not pure!"
+#
+# Input1:
+# 2
+# pure string
+# not_pure_string
+#
+# Output2:
+# pure string is pure.
+# not_pure_string is not pure!
+#
+# Input2:
+# 3
+# SoftUni
+# 12345
+# string.pureness
+#
+# Output2:
+# SoftUni is pure.
+# 12345 is pure.
+# string.pureness is not pure!
+
 number_n = int(input())
 
 for i in range (number_n):
