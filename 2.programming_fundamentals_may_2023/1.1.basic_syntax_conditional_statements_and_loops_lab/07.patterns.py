d3750720d899a5678c2032e5667f19f0d5028513
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-06-11 19:23:32 +0200

    Update 07.patterns.py

--- 2.programming_fundamentals_may_2023/1.1.basic_syntax_conditional_statements_and_loops_lab/07.patterns.py
+++ 2.programming_fundamentals_may_2023/1.1.basic_syntax_conditional_statements_and_loops_lab/07.patterns.py
@@ -1,3 +1,26 @@
+# Write a program that receives a number and creates the following pattern.
+# The number represents the largest count of stars on one row.
+#
+# Input1: 3
+# Output1:
+# *
+# **
+# ***
+# **
+# *
+#
+# Input2: 5
+# Output2:
+# *
+# **
+# ***
+# ****
+# *****
+# ****
+# ***
+# **
+# *
+
 n = int(input())
 
 for i in range (1, n + 1):
