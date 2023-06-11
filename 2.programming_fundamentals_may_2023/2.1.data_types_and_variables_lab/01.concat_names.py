7b2c1f85f6f8c23e553809149ab807b194815084
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-06-11 19:24:48 +0200

    Update 01.concat_names.py

--- 2.programming_fundamentals_may_2023/2.1.data_types_and_variables_lab/01.concat_names.py
+++ 2.programming_fundamentals_may_2023/2.1.data_types_and_variables_lab/01.concat_names.py
@@ -1,3 +1,26 @@
+# Write a program that reads two names and a delimiter. It should print the names joined by the delimiter.
+#
+# Input1:
+# John
+# Smith
+# ->
+#
+# Output1: John->Smith
+#
+# Input2:
+# Jan
+# White
+# <->
+#
+# Output2: Jan<->White
+#
+# Input3:
+# Linda
+# Terry
+# =>
+#
+# Output3: Linda=>Terry
+
 first_name = input()
 last_name = input()
 delimiter = input()
