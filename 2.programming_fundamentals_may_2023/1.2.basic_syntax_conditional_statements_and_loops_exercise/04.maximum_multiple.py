ab1c55ba7a92ab09b243fd15d8e2bc395c469f6a
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-06-11 19:24:12 +0200

    Update 04.maximum_multiple.py

--- 2.programming_fundamentals_may_2023/1.2.basic_syntax_conditional_statements_and_loops_exercise/04.maximum_multiple.py
+++ 2.programming_fundamentals_may_2023/1.2.basic_syntax_conditional_statements_and_loops_exercise/04.maximum_multiple.py
@@ -1,3 +1,29 @@
+# On the first line, you will be given a positive number, which will serve as a divisor.
+# On the second line, you will receive a positive number that will be the boundary.
+# You should find the largest integer N, that is:
+# • divisible by the given divisor
+# • less than or equal to the given bound
+# • greater than 0
+# Note: it is guaranteed that N is found.
+#
+# Input1:
+# 2
+# 7
+#
+# Output1: 6
+#
+# Input2:
+# 10
+# 50
+#
+# Output2: 50
+#
+# Input3:
+# 37
+# 200
+#
+# Output3: 185
+
 divisor = int(input())
 boundary = int(input())
 
