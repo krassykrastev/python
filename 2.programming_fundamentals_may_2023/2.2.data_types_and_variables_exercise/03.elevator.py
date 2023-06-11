1882069d0421ebfef6a79422b01ced898add1800
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-06-11 19:25:09 +0200

    Update 03.elevator.py

--- 2.programming_fundamentals_may_2023/2.2.data_types_and_variables_exercise/03.elevator.py
+++ 2.programming_fundamentals_may_2023/2.2.data_types_and_variables_exercise/03.elevator.py
@@ -1,3 +1,24 @@
+# Calculate how many courses will be needed to elevate N persons by using an elevator with a capacity of P persons.
+# The input holds two lines: the number of people N and the capacity P of the elevator.
+#
+# Input1:
+# 17
+# 3
+#
+# Output1: 6
+#
+# Input2:
+# 4
+# 5
+#
+# Output2: 1
+#
+# Input3:
+# 10
+# 5
+#
+# Output3: 2
+
 from math import ceil
 
 number_of_people = int(input())
