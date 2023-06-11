d2d47269edc81fb245d3250ab0115a69a5500c18
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-06-11 19:25:40 +0200

    Update 04.balanced_brackets.py

--- 2.programming_fundamentals_may_2023/2.3.data_types_and_variables_more_exercises/04.balanced_brackets.py
+++ 2.programming_fundamentals_may_2023/2.3.data_types_and_variables_more_exercises/04.balanced_brackets.py
@@ -1,3 +1,35 @@
+# On the first line, you will receive n – the number of lines, which will follow.
+# On the following n lines, you will receive one of the following:
+# • Opening bracket – "(",
+# • Closing bracket – ")" or
+# • Random string
+# Your task is to find out if the brackets are balanced. That means after every opening bracket should follow a closing one.
+# Nested parentheses are not valid, and if, for example, two consecutive opening brackets exist, the expression should be marked as unbalanced.
+# You should print "BALANCED" if the parentheses are balanced and "UNBALANCED" otherwise.
+#
+# Input1:
+# 8
+# (
+# 5 + 10
+# )
+# * 2 +
+# (
+# 5
+# )
+# -12
+# Output1: BALANCED
+#
+# Input2:
+# 6
+# 12 *
+# )
+# 10 + 2 -
+# (
+# 5 + 10
+# )
+#
+# Output2: UNBALANCED
+
 opening_bracket = False
 closing_bracket = False
 balanced = True
