e0a81aba1b6c81f075890851b766a79aacde9682
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-06-11 19:25:37 +0200

    Update 03.decrypting_messages.py

--- 2.programming_fundamentals_may_2023/2.3.data_types_and_variables_more_exercises/03.decrypting_messages.py
+++ 2.programming_fundamentals_may_2023/2.3.data_types_and_variables_more_exercises/03.decrypting_messages.py
@@ -1,3 +1,35 @@
+# On the first line, you will receive a key (integer).
+# On the second line, you will receive n – the number of lines, which will follow.
+# On the next n lines – you will receive a lower and an uppercase letter per line.
+# You should add the key to each of the characters and append them to a message.
+# In the end, print the decrypted message.
+#
+# Input1:
+# 3
+# 7
+# P
+# l
+# c
+# q
+# R
+# k
+# f
+#
+# Output1: SoftUni
+#
+# Input2:
+# 1
+# 7
+# C
+# d
+# b
+# q
+# x
+# o
+# s
+#
+# Output2: Decrypt
+
 string = ''
 decrypt_key = int(input())
 number_of_lines = int(input())
