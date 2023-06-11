dbf5478bebd8b3109af4f08b008eb844bc76cd6f
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-06-11 19:24:55 +0200

    Update 04.centuries_to_minutes.py

--- 2.programming_fundamentals_may_2023/2.1.data_types_and_variables_lab/04.centuries_to_minutes.py
+++ 2.programming_fundamentals_may_2023/2.1.data_types_and_variables_lab/04.centuries_to_minutes.py
@@ -1,3 +1,11 @@
+# Write a program that reads an integer number of centuries and converts it to years, days, hours, and minutes.
+#
+# Input1: 1
+# Output1: 1 centuries = 100 years = 36524 days = 876576 hours = 52594560 minutes
+#
+# Input2: 5
+# Output2: 5 centuries = 500 years = 182621 days = 4382904 hours = 262974240 minutes
+
 centuries = int(input())
 years = centuries * 100
 days = int(years * 365.2422)
