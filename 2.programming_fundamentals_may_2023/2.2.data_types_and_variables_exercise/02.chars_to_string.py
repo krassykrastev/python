214ca5bedef51d5fe88b9f4d0d4789920cd8bf49
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-06-11 19:25:07 +0200

    Update 02.chars_to_string.py

--- 2.programming_fundamentals_may_2023/2.2.data_types_and_variables_exercise/02.chars_to_string.py
+++ 2.programming_fundamentals_may_2023/2.2.data_types_and_variables_exercise/02.chars_to_string.py
@@ -1,3 +1,27 @@
+# Write a function that receives 3 characters.
+# Concatenate all the characters into one string and print it on the console.
+#
+# Input1:
+# a
+# b
+# c
+#
+# Output1: abc
+#
+# Input2:
+# %
+# 2
+# o
+#
+# Output2: %2o
+#
+# Input3:
+# 1
+# 5
+# p
+#
+# Output3: 15p
+
 first_character = input()
 second_character = input()
 third_character = input()
