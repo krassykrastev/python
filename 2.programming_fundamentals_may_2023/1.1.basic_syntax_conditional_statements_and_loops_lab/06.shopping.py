a5f5e3cd36975274f62630f7b8ff35b312ecb045
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-06-11 19:23:29 +0200

    Update 06.shopping.py

--- 2.programming_fundamentals_may_2023/1.1.basic_syntax_conditional_statements_and_loops_lab/06.shopping.py
+++ 2.programming_fundamentals_may_2023/1.1.basic_syntax_conditional_statements_and_loops_lab/06.shopping.py
@@ -1,3 +1,23 @@
+# Write a program that reads an integer number representing a budget.
+# On the following lines, it reads integer numbers representing the prices of each product you should buy until it receives the command "End".
+# During the iterations, if there is not enough budget left to buy the next product, it prints "You went in overdraft!" and end the program.
+# Otherwise, if you accomplished to buy all products before receiving "End", it prints "You bought everything needed."
+#
+# Input1:
+# 100
+# 5
+# End
+#
+# Output1: You bought everything needed.
+#
+# Input2:
+# 50
+# 25
+# 20
+# 10
+#
+# Output2: You went in overdraft!
+
 budget = int(input())
 total_price = 0
 
