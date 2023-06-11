da190d5de9d982d70559dc05350153322b5e49dd
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-06-11 19:24:53 +0200

    Update 03.pounds_to_dollars.py

--- 2.programming_fundamentals_may_2023/2.1.data_types_and_variables_lab/03.pounds_to_dollars.py
+++ 2.programming_fundamentals_may_2023/2.1.data_types_and_variables_lab/03.pounds_to_dollars.py
@@ -1,3 +1,12 @@
+# Write a program that converts British pounds (integer) to US dollars formatted to the 3rd decimal point.
+# 1 British Pound = 1.31 Dollars.
+#
+# Input1: 80
+# Output1: 104.800
+#
+# Input2: 39
+# Output2: 51.090
+
 pound = int(input())
 dollars = pound * 1.31
 
