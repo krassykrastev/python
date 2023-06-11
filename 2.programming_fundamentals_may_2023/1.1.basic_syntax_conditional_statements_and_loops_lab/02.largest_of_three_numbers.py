381f6ba5a21268b857d921ea3b49eb368f247b24
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-06-11 19:23:03 +0200

    Update 02.largest_of_three_numbers.py

--- 2.programming_fundamentals_may_2023/1.1.basic_syntax_conditional_statements_and_loops_lab/02.largest_of_three_numbers.py
+++ 2.programming_fundamentals_may_2023/1.1.basic_syntax_conditional_statements_and_loops_lab/02.largest_of_three_numbers.py
@@ -1,3 +1,19 @@
+# Write a program that receives three whole numbers and prints the largest one.
+#
+# Input1:
+# 3
+# -1
+# 5
+#
+# Output1: 5
+#
+# Input2:
+# 0
+# -1
+# -2
+#
+# Output2: 0
+
 num1 = int(input())
 num2 = int(input())
 num3 = int(input())
