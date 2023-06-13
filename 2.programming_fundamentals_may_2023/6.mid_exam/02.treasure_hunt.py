a5d3f17b17a6fa313b08db62406197addbfbba2f
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-06-13 10:52:51 +0200

    Update 02.treasure_hunt.py

--- 2.programming_fundamentals_may_2023/6.mid_exam/02.treasure_hunt.py
+++ 2.programming_fundamentals_may_2023/6.mid_exam/02.treasure_hunt.py
@@ -81,34 +81,3 @@ if initial_loot:
     print(f"Average treasure gain: {average_treasure_gain:.2f} pirate credits.")
 else:
     print("Failed treasure hunt.")
-
-# initial_loot = input().split("|")
-#
-# data_info = input()
-#
-# while data_info != "Yohoho!":
-#     command, *data = [x for x in data_info.split()]
-#
-#     if command == "Loot":
-#         for item in data:
-#             if item not in initial_loot:
-#                 initial_loot.insert(0, item)
-#
-#     elif command == "Drop":
-#         index = int(data[0])
-#         if 0 <= index < len(initial_loot):
-#             initial_loot.append(initial_loot.pop(index))
-#
-#     elif command == "Steal":
-#         count = int(data[0])
-#         stolen_items = initial_loot[-count:]
-#         initial_loot = initial_loot[:-count]
-#         print(*stolen_items, sep=", ")
-#     data_info = input()
-#
-#
-# if initial_loot:
-#     average_treasure_gain = sum(len(x) for x in initial_loot) / len(initial_loot)
-#     print(f"Average treasure gain: {average_treasure_gain:.2f} pirate credits.")
-# else:
-#     print("Failed treasure hunt.")
