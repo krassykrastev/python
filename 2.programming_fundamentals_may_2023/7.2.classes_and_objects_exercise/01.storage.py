3cf66b29ece8b306ea1fc0fb850064ace6baaed1
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-06-23 22:38:38 +0200

    Update 01.storage.py

--- 2.programming_fundamentals_may_2023/7.2.classes_and_objects_exercise/01.storage.py
+++ 2.programming_fundamentals_may_2023/7.2.classes_and_objects_exercise/01.storage.py
@@ -1,3 +1,9 @@
+# Create a class Storage. The __init__ method should accept one parameter - the capacity of the storage.
+# The Storage class should also have an attribute called storage - empty list, where all the items will be stored.
+# The class should have two additional methods:
+# • add_product(product: str) - adds the product in the storage if there is enough space for it
+# • get_products() - returns the storage list
+
 class Storage:
     storage = []
     def __init__(self, capacity):
