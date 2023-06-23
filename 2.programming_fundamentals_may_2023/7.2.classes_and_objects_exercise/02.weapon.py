d4363f74f46f09e245b730974ea46b17332beb0a
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-06-23 22:39:40 +0200

    Update 02.weapon.py

--- 2.programming_fundamentals_may_2023/7.2.classes_and_objects_exercise/02.weapon.py
+++ 2.programming_fundamentals_may_2023/7.2.classes_and_objects_exercise/02.weapon.py
@@ -1,3 +1,11 @@
+# Create a class Weapon. The __init__ method should receive a number of bullets (integer).
+# Create an attribute called bullets to store that number. The class should also have the following methods:
+# • shoot()
+#   o If there are bullets in the weapon, reduce them by 1 and return a message "shooting..."
+#   o If there are no bullets left, return: "no bullets left"
+# • __repr__()
+#   o Returns "Remaining bullets: {amount_of_bullets}"
+
 class Weapon:
     def __init__(self, bullets: int):
         self.bullets = bullets
