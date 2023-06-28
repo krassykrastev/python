7b631138df6a520b527c41ec690e651078c471b9
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-06-28 23:09:14 +0200

    Update 01.bakery.py

--- 2.programming_fundamentals_may_2023/9.1.dictionaries_lab/01.bakery.py
+++ 2.programming_fundamentals_may_2023/9.1.dictionaries_lab/01.bakery.py
@@ -2,6 +2,12 @@
 # You will receive a single line containing some food (keys) and quantities (values). They will be separated by a single
 # space (the first element is the key, the second – the value, and so on).
 # Create a dictionary with all the keys and values and print it on the console.
+#
+# Input1: bread 10 butter 4 sugar 9 jam 12
+# Output1: {'bread': 10, 'butter': 4, 'sugar': 9, 'jam': 12}
+#
+# Input2: eggs 3 sugar 7 salt 1 butter 3
+# Output2: {'eggs': 3, 'sugar': 7, 'salt': 1, 'butter': 3}
 
 data = input().split()
 
