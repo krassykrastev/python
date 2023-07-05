7d1f03f598b27c7ef633573ea0bc4d226c47b013
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-07-05 23:11:50 +0200

    Update 01.reverse_strings.py

--- 2.programming_fundamentals_may_2023/10.1.text_processing/01.reverse_strings.py
+++ 2.programming_fundamentals_may_2023/10.1.text_processing/01.reverse_strings.py
@@ -1,5 +1,27 @@
 # You will be given strings on separate lines until you receive an "end" command.
 # Write a program that reverses strings and prints each pair on a separate line in the format "{word} = {reversed_word}".
+#
+# Input1:
+# helLo
+# Softuni
+# bottle
+# end
+#
+# Output1:
+# helLo = oLleh
+# Softuni = inutfoS
+# bottle = elttob
+#
+# Input2:
+# Dog
+# caT
+# chAir
+# end
+#
+# Output2:
+# Dog = goD
+# caT = Tac
+# chAir = riAhc
 
 while True:
     command = input()
