fe183a10941f445570d67f3e733ffaacb2982254
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-07-07 21:31:07 +0200

    Create 03.substring.py

---
+++ 2.programming_fundamentals_may_2023/10.1.text_processing_lab/03.substring.py
@@ -0,0 +1,17 @@
+# On the first line, you will receive a string. On the second line, you will receive a second string.
+# Write a program that removes all the occurrences of the first string in the second until there is no match.
+# At the end, print the remaining string.
+#
+# Input1:
+# ice
+# kicegiciceeb
+#
+# Output1: kgb
+
+first_string = input()
+second_string = input()
+
+while first_string in second_string:
+    second_string = second_string.replace(first_string, "")
+
+print(second_string)
