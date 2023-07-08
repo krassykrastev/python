a613985a47b4ed77aa35c54df232ce60de7cd5ae
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-07-08 12:23:50 +0200

    Update 01.extract_person_information.py

--- 2.programming_fundamentals_may_2023/10.3.text_processing_more_exercises/01.extract_person_information.py
+++ 2.programming_fundamentals_may_2023/10.3.text_processing_more_exercises/01.extract_person_information.py
@@ -45,3 +45,20 @@ for line in range(lines_of_string):
 
     print(f"{name} is {age} years old.")
     name = age = ""
+
+# another solution
+# def get_name_and_age(sentence):
+#     name_first_index = sentence.index("@") + 1
+#     name_last_index = sentence.index("|")
+#     age_first_index = sentence.index("#") + 1
+#     age_last_index = sentence.index("*")
+#     name = sentence[name_first_index:name_last_index]
+#     age = sentence[age_first_index:age_last_index]
+#     return name, age
+#
+#
+# number_of_entries = int(input())
+# for sentence in range(number_of_entries):
+#     current_sentence = input()
+#     current_name, current_age = get_name_and_age(current_sentence)
+#     print(f"{current_name} is {current_age} years old.")
