5124be0d590ebea0a75d0a5b32eb665e3cbf5bda
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-07-15 15:04:36 +0200

    Create 03.star_enigma.py

---
+++ 2.programming_fundamentals_may_2023/11.3.regular_expressions_more_exercises/03.star_enigma.py
@@ -0,0 +1,81 @@
+# The war is in its peak, but you, young Padawan, can turn the tides with your programming skills.
+# You are tasked to create a program to decrypt the messages of The Order and prevent the death of hundreds
+# of lives.
+# You will receive several messages, which are encrypted using the legendary star enigma. You should decrypt
+# the messages, following these rules:
+# To properly decrypt a message, you should count all the letters [s, t, a, r] – case insensitive and remove
+# the count from the current ASCII value of each symbol of the encrypted message.
+# After decryption:
+# Each message should have a planet name, population, attack type ('A', as attack or 'D', as destruction)
+# and soldier count.
+# The planet name starts after '@' and contains only letters from the Latin alphabet.
+# The planet population starts after ':' and is an Integer;
+# The attack type may be "A"(attack) or "D"(destruction) and must be surrounded by "!" (exclamation mark).
+# The soldier count starts after "->" and should be an Integer.
+# The order in the message should be: planet name -> planet population -> attack type -> soldier count.
+# Each part can be separated from the others by any character except: '@', '-', '!', ':' and '>'.
+#
+# Input1:
+# 2
+# STCDoghudd4=63333$D$0A53333
+# EHfsytsnhf?8555&I&2C9555SR
+#
+# Output1:
+# Attacked planets: 1
+# -> Alderaa
+# Destroyed planets: 1
+# -> Cantonica
+#
+# Input2:
+# 3
+# tt(''DGsvywgerx>6444444444%H%1B9444
+# GQhrr|A977777(H(TTTT
+# EHfsytsnhf?8555&I&2C9555SR
+#
+# Output2:
+# Attacked planets: 0
+# Destroyed planets: 2
+# -> Cantonica
+# -> Coruscant
+
+import re
+
+number_of_messages = int(input())
+attacked_planets = []
+destroyed_planets = []
+
+for message in range(number_of_messages):
+    current_message = input()
+    encryption_pattern = "[star]"
+    decryption_key = len(re.findall(encryption_pattern, current_message, re.IGNORECASE))
+    decrypted_message = ""
+
+    for symbol in current_message:
+        decrypted_message += chr(ord(symbol) - decryption_key)
+
+    validation_pattern = r"@[A-Za-z]+[^@\-\!\:\>]*\:\d+[^@\-\!\:\>]*\![AD]\![^@\-\!\:\>]*\-\>\d+"
+    validation = re.findall(validation_pattern, decrypted_message)
+
+    if validation:
+        name_pattern = "@[A-Za-z]+"
+        attack_pattern = "![AD]!"
+        planet_name = re.findall(name_pattern, decrypted_message)[0][1:]
+        attack_type = re.findall(attack_pattern, decrypted_message)[0][1]
+
+        if attack_type == "A":
+            attacked_planets.append(planet_name)
+
+        elif attack_type == "D":
+            destroyed_planets.append(planet_name)
+
+print(f"Attacked planets: {len(attacked_planets)}")
+
+if len(attacked_planets) > 0:
+    for planet in sorted(attacked_planets):
+        print(f"-> {planet}")
+
+print(f"Destroyed planets: {len(destroyed_planets)}")
+
+if len(destroyed_planets) > 0:
+    for planet in sorted(destroyed_planets):
+        print(f"-> {planet}")
