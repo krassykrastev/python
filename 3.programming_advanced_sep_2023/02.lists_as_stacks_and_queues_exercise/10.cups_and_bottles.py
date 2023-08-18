b5dfc2cd206df33b1c22e9a14d24d726bcf84d11
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-08-18 09:26:59 +0200

    Update 10.cups_and_bottles.py

--- 3.programming_advanced_sep_2023/02.lists_as_stacks_and_queues_exercise/10.cups_and_bottles.py
+++ 3.programming_advanced_sep_2023/02.lists_as_stacks_and_queues_exercise/10.cups_and_bottles.py
@@ -53,11 +53,13 @@ bottles_capacity = deque(map(int, input().split()))
 wasted_litters_of_water = 0
 
 while cups_capacity and bottles_capacity:
+
     current_cup = cups_capacity.popleft()
     current_bottle = bottles_capacity.pop()
 
     if current_bottle >= current_cup:
         wasted_litters_of_water += current_bottle - current_cup
+
     else:
         cups_capacity.appendleft(current_cup - current_bottle)
 
