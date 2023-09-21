9ce4d4206c08419f1201130db0781ade51e0b234
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-09-21 08:07:22 +0200

    Update 04.honey.py

--- 3.programming_advanced_sep_2023/2.3.tuples_and_sets_exercise_2/04.honey.py
+++ 3.programming_advanced_sep_2023/2.3.tuples_and_sets_exercise_2/04.honey.py
@@ -47,3 +47,35 @@
 # Total honey made: 4500
 # Nectar left: 15, 9, 5
 
+from collections import deque
+
+bees = deque(int(x) for x in input().split())
+nectar = [int(x) for x in input().split()]
+symbols = deque(input().split())
+total_honey = 0
+
+while bees and nectar:
+    if nectar[-1] >= bees[0]:
+        symbol = symbols.popleft()
+        collected_nectar = nectar.pop()
+        bee = bees.popleft()
+        if collected_nectar == 0:
+            continue
+        if symbol == "+":
+            total_honey += abs(bee + collected_nectar)
+        elif symbol == "-":
+            total_honey += abs(bee - collected_nectar)
+        elif symbol == "*":
+            total_honey += abs(bee * collected_nectar)
+        else:
+            total_honey += abs(bee / collected_nectar)
+    else:
+        nectar.pop()
+
+print(f"Total honey made: {total_honey}")
+
+if bees:
+    print(f"Bees left: {', '.join(map(str, bees))}")
+
+if nectar:
+    print(f"Nectar left: {', '.join(map(str, nectar))}")
