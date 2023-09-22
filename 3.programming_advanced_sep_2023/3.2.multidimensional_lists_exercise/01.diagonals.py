7074758ff20a7f39a87507e33970647153b53ca5
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-09-22 12:26:54 +0200

    Update 01.diagonals.py

--- 3.programming_advanced_sep_2023/3.2.multidimensional_lists_exercise/01.diagonals.py
+++ 3.programming_advanced_sep_2023/3.2.multidimensional_lists_exercise/01.diagonals.py
@@ -26,7 +26,7 @@ primary_diagonal = []
 secondary_diagonal = []
 for i in range(n):
     primary_diagonal.append(matrix[i][i])
-    secondary_diagonal.append(matrix[i][n - i - 1])
+    secondary_diagonal.append(matrix[i][- i - 1])
 
 print(f"Primary diagonal: {', '.join(str(x) for x in primary_diagonal)}. Sum: {sum(primary_diagonal)}")
 print(f"Secondary diagonal: {', '.join(str(x) for x in secondary_diagonal)}. Sum: {sum(secondary_diagonal)}")
