e364fc2d2145d0e8139e2e42ae3f1dc4ccd62c4a
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-09-22 12:46:06 +0200

    Update 01.diagonals.py

--- 3.programming_advanced_sep_2023/3.2.multidimensional_lists_exercise/01.diagonals.py
+++ 3.programming_advanced_sep_2023/3.2.multidimensional_lists_exercise/01.diagonals.py
@@ -24,6 +24,7 @@ for row_index in range(n):
 
 primary_diagonal = []
 secondary_diagonal = []
+
 for i in range(n):
     primary_diagonal.append(matrix[i][i])
     secondary_diagonal.append(matrix[i][- i - 1])
