9dc67c11dc445404d1247301a7a515b3a7383666
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-10-05 21:46:20 +0200

    Create 01.file_handling.py

---
+++ 3.programming_advanced_sep_2023/6.1.file_handling_lab/01.file_handling.py
@@ -0,0 +1,12 @@
+# You are given a file called text.txt with the following text:
+# This is some random line
+# This is the second line
+# And this is the third one
+# Create a program that opens the file. If the file is found, print 'File found'. If the file is not found, print
+# 'File not found'.
+
+try:
+    file = open("text.txt", "r")
+    print("Found")
+except FileNotFoundError:
+    print("File not found")
