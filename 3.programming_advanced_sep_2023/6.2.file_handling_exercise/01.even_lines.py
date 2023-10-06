dc0c2b25fd229610b82dfb6a667febb4987bccaa
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-10-06 12:12:49 +0200

    Create 01.even_lines.py

---
+++ 3.programming_advanced_sep_2023/6.2.file_handling_exercise/01.even_lines.py
@@ -0,0 +1,12 @@
+# Write a program that reads a text file and prints on the console its even lines. Line numbers start from 0. Before you
+# print the result, replace {"-", ",", ".", "!", "?"} with "@" and reverse the order of the words.
+#
+# text.txt:
+# -I was quick to judge him, but it wasn't his fault.
+# -Is this some kind of joke?! Is it?
+# -Quick, hide here. It is safer.
+#
+# Output:
+# fault@ his wasn't it but him@ judge to quick was @I
+# safer@ is It here@ hide @Quick@
+
