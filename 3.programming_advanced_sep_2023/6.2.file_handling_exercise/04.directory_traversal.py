e3aeeea93b7aa8ac8982eef21449c8570e7a4bc3
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-10-06 16:21:07 +0200

    Update 04.directory_traversal.py

--- 3.programming_advanced_sep_2023/6.2.file_handling_exercise/04.directory_traversal.py
+++ 3.programming_advanced_sep_2023/6.2.file_handling_exercise/04.directory_traversal.py
@@ -6,24 +6,26 @@
 import os
 
 files = {}
-directory = "./04.directory_traversal/"
+directory = "../"
 
-for element in os.listdir(directory):
-    f = os.path.join(directory, element)
-    if os.path.isfile(f):
-        ext = element.split(".")[-1]
-        if ext not in files:
-            files[ext] = []
-        files[ext].append(element)
+
+def get_files(folder, level):
+    if level < 0:
+        return
+
+    for element in os.listdir(folder):
+        f = os.path.join(folder, element)
+        if os.path.isfile(f):
+            extension = element.split(".")[-1]
+            if extension not in files:
+                files[extension] = []
+            files[extension].append(element)
 
     else:
-        for el in os.listdir(f):
-            filename = os.path.join(f, el)
-            if os.path.isfile(filename):
-                ext = el.split(".")[-1]
-                if ext not in files:
-                    files[ext] = []
-                files[ext].append(el)
+        get_files(f, level - 1)
+
+
+get_files(directory, 1)
 
 with open(os.path.join(directory, "report.txt"), "w") as output_file:
     for ext, f_names in sorted(files.items()):
