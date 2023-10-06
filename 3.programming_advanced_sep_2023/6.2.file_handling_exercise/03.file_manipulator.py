6ac726493922adcf337a9da52fdacdbd0c1b051c
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-10-06 15:07:50 +0200

    Update 03.file_manipulator.py

--- 3.programming_advanced_sep_2023/6.2.file_handling_exercise/03.file_manipulator.py
+++ 3.programming_advanced_sep_2023/6.2.file_handling_exercise/03.file_manipulator.py
@@ -18,3 +18,36 @@
 # Delete-file.txt
 # End
 
+import os
+
+while True:
+    line = input()
+    if line == "End":
+        break
+    command, filename, *args = line.split("-")
+
+    if command == "Create":
+        open(filename, "w").close()
+
+    elif command == "Add":
+        with open(filename, "a") as file:
+            file.write(f"{args[0]}\n")
+
+    elif command == "Replace":
+        try:
+            with open(filename, "r") as file:
+                content = file.read()
+
+        except FileNotFoundError:
+            print("An error occurred")
+
+        else:
+            with open(filename, "w") as file:
+                content = content.replace(args[0], args[1])
+                file.write(content)
+
+    elif command == "Delete":
+        if os.path.exists(filename):
+            os.remove(filename)
+        else:
+            print("An error occurred")
