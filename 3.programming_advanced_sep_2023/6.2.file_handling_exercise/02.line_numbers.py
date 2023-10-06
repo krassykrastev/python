09efb934e64de6939f819c9b7d8eaa98f575dac5
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-10-06 12:27:59 +0200

    Update 02.line_numbers.py

--- 3.programming_advanced_sep_2023/6.2.file_handling_exercise/02.line_numbers.py
+++ 3.programming_advanced_sep_2023/6.2.file_handling_exercise/02.line_numbers.py
@@ -6,8 +6,9 @@
 # -Is this some kind of joke?! Is it?
 # -Quick, hide here. It is safer.
 #
-# Output:
+# output2.txt:
 # Line 1: -I was quick to judge him, but it wasn't his fault. (37)(4)
 # Line 2: -Is this some kind of joke?! Is it? (24)(4)
 # Line 3: -Quick, hide here. It is safer. (22)(4)
 
+with open("text2.txt", "r") as file:
