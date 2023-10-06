f3423b6903cb59fef03544210cf1d404677f88d9
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-10-06 12:40:31 +0200

    Update 02.line_numbers.py

--- 3.programming_advanced_sep_2023/6.2.file_handling_exercise/02.line_numbers.py
+++ 3.programming_advanced_sep_2023/6.2.file_handling_exercise/02.line_numbers.py
@@ -11,4 +11,17 @@
 # Line 2: -Is this some kind of joke?! Is it? (24)(4)
 # Line 3: -Quick, hide here. It is safer. (22)(4)
 
-with open("text2.txt", "r") as file:
+from string import punctuation
+
+with open("text2.txt", "r") as input_file, open("output.txt", "w") as output_file:
+    result = []
+    for row, line in enumerate(input_file):
+        letters_count = 0
+        punc_count = 0
+        for char in line:
+            if char.isalpha():
+                letters_count += 1
+            elif char in punctuation:
+                punc_count += 1
+        result.append(f"Line {row + 1}: {line.strip()} ({letters_count})({punc_count})")
+    output_file.write("\n".join(result))
