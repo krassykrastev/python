fcde0a784aad721cda1e99c8a6a35c7bb3e08904
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-10-11 11:23:08 +0200

    update

---
+++ 3.programming_advanced_sep_2023/9.1.modules/03.triangle/main.py
@@ -0,0 +1,7 @@
+# Create a module for printing a 03.triangle. You will receive an integer number which is the size of the 03.triangle.
+
+from core.triangle_printing import print_triangle
+
+n = int(input())
+
+print_triangle(n)
