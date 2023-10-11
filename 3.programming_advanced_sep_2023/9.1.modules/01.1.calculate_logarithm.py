56b0abbcf1db91d6f14a9793c50f734714f3cb8f
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-10-11 10:16:22 +0200

    Update 01.1.calculate_logarithm.py

--- 3.programming_advanced_sep_2023/9.1.modules/01.1.calculate_logarithm.py
+++ 3.programming_advanced_sep_2023/9.1.modules/01.1.calculate_logarithm.py
@@ -13,3 +13,12 @@
 # Output2:
 # 1.00
 
+from math import log
+
+number = int(input())
+command = input()
+
+if command == "natural":
+    print(f"{log(number):.2f}")
+else:
+    print(f"{log(number, int(command)):.2f}")
