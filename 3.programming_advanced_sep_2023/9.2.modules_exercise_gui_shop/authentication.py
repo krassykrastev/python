931f52bac5af57165d5b942ed4351481223c6a3f
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-10-14 11:12:21 +0200

    Update authentication.py

--- 3.programming_advanced_sep_2023/9.2.modules_exercise_gui_shop/authentication.py
+++ 3.programming_advanced_sep_2023/9.2.modules_exercise_gui_shop/authentication.py
@@ -1,2 +1,7 @@
+import tkinter as tk
+from canvas import app
+
+
 def render_main_enter_screen():
-    pass
+    tk.Button(app, text="Login", bg="green", fg="white").grid(row=0, column=0)
+    tk.Button(app, text="Register", bg="yellow", fg="black").grid(row=0, column=1)
