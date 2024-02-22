e9f584a4e0b11b5f084be8387df5bd5d630811ca
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2024-02-22 16:35:17 +0100

    Initial Commit

---
+++ 6.js_front_end_feb_2024/2.js_syntax_fundamentals_exercise/07.chars_to_string.js
@@ -0,0 +1,9 @@
+// Write a function, which receives 3 parameters. Each parameter is a single character. Combine all the characters into one string and print it on the console.
+
+function solve(char1, char2, char3) {
+    console.log(char1 + char2 + char3);
+}
+
+solve('a','b','c')
+solve('%','2','o')
+solve('1','5','p')
