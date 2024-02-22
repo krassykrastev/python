91ed8d61b5232e0c8de5e43f8ad714c35544bd6a
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2024-02-22 16:58:32 +0100

    Initial Commit

---
+++ 6.js_front_end_feb_2024/2.js_syntax_fundamentals_exercise/08.reversed_chars.js
@@ -0,0 +1,9 @@
+// Write a program that takes 3 parameters (characters) and prints them in reversed order with a space between them.
+
+
+function solve(char1, char2, char3) {
+    console.log(char3 + ' ' + char2 + ' ' + char1);
+}
+
+solve('A','B','C')
+solve('1','L','&')
---
+++ 6.js_front_end_feb_2024/2.js_syntax_fundamentals_exercise/09.fruit.js
@@ -0,0 +1,14 @@
+// Write a function that calculates how much money you need to buy fruit. You will receive a string for the type of fruit you want to buy, a number for weight in grams, and another number for the price per kilogram. 
+// Print the following text on the console:  
+// `I need ${money} to buy {weight} kilograms {fruit}.`
+// Print the weight and the money rounded to two decimal places.
+// The input comes as three arguments passed to your function.
+// The output should be printed on the console.
+
+function solve(fruit, weight, pricePerKg) {
+    let money = (weight / 1000 * pricePerKg).toFixed(2);
+    console.log(`I need $${money} to buy ${weightInKg.toFixed(2)} kilograms ${fruit}.`);
+}
+
+solve('orange', 2500, 1.80)
+solve('apple', 1563, 2.35)
