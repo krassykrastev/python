2fd2077e125e778bd2b289149efdfa0e627a9445
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2024-03-03 16:30:16 +0100

    Initial Commit

---
+++ 6.js_front_end_feb_2024/2.2.js_arrays_and_strings_exercise/01.array_rotation.js
@@ -0,0 +1,11 @@
+function solve(numbers, rotations) {
+    for (let i = 0; i < rotations; i++) {
+        firstElement = numbers.shift()
+        numbers.push(firstElement);
+    }
+    console.log(numbers.join(' '));
+}
+
+solve([51, 47, 32, 61, 21], 2)
+solve([32, 21, 61, 1], 4)
+solve([2, 4, 15, 31], 5)
