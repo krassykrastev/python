5959eb3232f4590bf4becca87a5f1ef43b69ab69
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2024-03-11 12:12:16 +0100

    update

--- 6.js_front_end_feb_2024/3.1.js_functions_and_statements/01.format_grade.js
+++ 6.js_front_end_feb_2024/3.1.js_functions_and_statements/01.format_grade.js
@@ -1,20 +1,21 @@
 function solve(grade) {
-    let formatttedGrade = ''
     if (grade < 3) {
-        formatttedGrade = `Fail (${grade})`
-    } else if (grade < 3.5) {
-        formatttedGrade = `Poor (${grade.toFixed(2)})`
-    } else if (grade < 4.5) {
-        formatttedGrade = `Good (${grade.toFixed(2)})`
-    } else if (grade < 5.5) {
-        formatttedGrade = `Very good (${grade.toFixed(2)})`
-    } else {
-        formatttedGrade = `Excellent (${grade.toFixed(2)})`
+        return `Fail (${grade})`
+    } 
+    if (grade < 3.5) {
+        return `Poor (${grade.toFixed(2)})`
+    } 
+    if (grade < 4.5) {
+        return `Good (${grade.toFixed(2)})`
+    } 
+    if (grade < 5.5) {
+        return `Very good (${grade.toFixed(2)})`
+    } 
+    else {
+        return `Excellent (${grade.toFixed(2)})`
     }
-
-    console.log(formatttedGrade)
 }
 
-solve(3.33)
-solve(4.50)
-solve(2.99)
+console.log(solve(3.33))
+console.log(solve(4.50))
+console.log(solve(2.99))
