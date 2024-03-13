70fd54f28fed4adcb3a531453519d6a92c487140
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2024-03-13 15:34:44 +0100

    Initial Commit

---
+++ 6.js_front_end_feb_2024/3.2.js_functions_and_statements_exercise/02.add_and_subtract.js
@@ -0,0 +1,9 @@
+function solve(first, second, third) {
+    let sum = first + second;
+    let result = sum - third;
+    console.log(result);
+}
+
+solve(23, 6, 10); // 19
+solve(1, 17, 30); // -12
+solve(42, 58, 100); // 0
