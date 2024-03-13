b348c0918b126872bda740b2806f5031cefe99c4
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2024-03-13 15:15:56 +0100

    Initial Commit

---
+++ 6.js_front_end_feb_2024/3.2.js_functions_and_statements_exercise/01.smallest_of_three_numbers.js
@@ -0,0 +1,9 @@
+function solve(a, b, c) {
+    let smallest = Math.min(a, b, c);
+    console.log(smallest);
+}
+
+solve(2, 5, 3); // 2
+solve(600, 342, 123); // 123
+solve(25, 21, 4); // 4
+solve(2, 2, 2); // 1
