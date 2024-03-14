712224f5224ecbd8d837f9749237a9e8d4075f1c
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2024-03-14 12:18:25 +0100

    Initial Commit

---
+++ 6.js_front_end_feb_2024/4.1.js_objects_and_classes_lab/01.person_info.js
@@ -0,0 +1,11 @@
+function solve(firstName, lastName, age) {
+    let person = {
+        firstName: firstName,
+        lastName: lastName,
+        age: age,
+    };
+
+    return person;
+}
+
+console.log(solve("Peter", "Pan", "20"));
