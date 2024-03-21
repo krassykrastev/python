ba0714e018c111923c52d5f458ee704d5671ff65
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2024-03-21 15:28:20 +0100

    Initial Commit

--- 6.js_front_end_feb_2024/5.1.js_dom_introduction _lab/03.sum_numbers/SumNumbers.js
+++ 6.js_front_end_feb_2024/5.1.js_dom_introduction _lab/03.sum_numbers/SumNumbers.js
@@ -1,3 +1,11 @@
 function calc() {
-    // TODO: sum = num1 + num2
+    const firstInputElement = document.getElementById('num1');
+    const secondInputElement = document.getElementById('num2');
+    const sumInputElemtn = document.getElementById('sum');
+
+    const firstNumnber = Number(firstInputElement.value);
+    const secondNumber = Number(secondInputElement.value);
+
+    const sum = firstNumnber + secondNumber;
+    sumInputElemtn.value = sum;
 }
