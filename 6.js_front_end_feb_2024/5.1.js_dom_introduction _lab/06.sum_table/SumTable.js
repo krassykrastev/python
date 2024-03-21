758c162aea0b64392b30c8475e3cfa22da9fec87
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2024-03-21 15:48:59 +0100

    Initial Commit

--- 6.js_front_end_feb_2024/5.1.js_dom_introduction _lab/06.sum_table/SumTable.js
+++ 6.js_front_end_feb_2024/5.1.js_dom_introduction _lab/06.sum_table/SumTable.js
@@ -1,3 +1,10 @@
 function sumTable() {
+    const tdCostElements = document.querySelectorAll('table tr td:last-of-type:not(#sum)');
+    const tdSumElement = document.getElementById('sum');
 
+    const sum = Array
+    .from(tdCostElements)
+    .reduce((sum, element) => sum + Number(element.textContent), 0); 
+    
+    tdSumElement.textContent = sum;
 }
