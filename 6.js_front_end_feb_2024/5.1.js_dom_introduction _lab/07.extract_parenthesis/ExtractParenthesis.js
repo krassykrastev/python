75dabae08d4501a408ee9e4042d951770c2c6672
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2024-03-21 15:59:59 +0100

    Initial Commit

--- 6.js_front_end_feb_2024/5.1.js_dom_introduction _lab/07.extract_parenthesis/ExtractParenthesis.js
+++ 6.js_front_end_feb_2024/5.1.js_dom_introduction _lab/07.extract_parenthesis/ExtractParenthesis.js
@@ -1,3 +1,11 @@
 function extract(content) {
+    const contentElement = document.getElementById(content);
+    const matches = contentElement.textContent.matchAll(/\(([a-zA-Z ]+)\)/g);
 
+    const text = Array
+        .from(matches)
+        .map(match => match[1])
+        .join('; ');
+
+    return text;
 }
