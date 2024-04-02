5275db0f7bb6efcc0574423f6b68a76a69129f9f
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2024-04-02 16:05:38 +0200

    Initial Commit

--- 6.js_front_end_feb_2024/5.2.js_dom_introduction_exercise/01.subtraction/subtract.js
+++ 6.js_front_end_feb_2024/5.2.js_dom_introduction_exercise/01.subtraction/subtract.js
@@ -1,3 +1,10 @@
 function subtract() {
-    console.log('TODO:...');
+    const firstNumberElement = document.getElementById('firstNumber');
+    const secondNumberElement = document.getElementById('secondNumber');
+    const resultElement = document.getElementById('result');
+
+    const firstNumber = Number(firstNumberElement.value);
+    const secondNumber = Number(secondNumberElement.value);
+
+    resultElement.textContent = firstNumber - secondNumber;
 }
