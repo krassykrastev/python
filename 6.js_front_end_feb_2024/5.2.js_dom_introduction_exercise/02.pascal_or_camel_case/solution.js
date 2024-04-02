87915016e3f98986682027563d24b40c4c1e35e7
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2024-04-02 16:22:36 +0200

    Initial Commit

--- 6.js_front_end_feb_2024/5.2.js_dom_introduction_exercise/02.pascal_or_camel_case/solution.js
+++ 6.js_front_end_feb_2024/5.2.js_dom_introduction_exercise/02.pascal_or_camel_case/solution.js
@@ -1,3 +1,29 @@
 function solve() {
-  //TODO...
+  const textElement = document.getElementById('text');
+  const namingConventionElement = document.getElementById('naming-convention');
+  const resultElement = document.getElementById('result');
+
+  const text = textElement.value;
+  const namingConvention = namingConventionElement.value;
+
+  const convertor = {
+    'Pascal Case': (text) => text
+      .toLowerCase()
+      .split(' ')
+      .map(word => word[0].toUpperCase() + word.slice(1))
+      .join(''),
+    'Camel Case': (text) => text
+      .toLowerCase()
+      .split(' ')
+      .map((word, index) => index === 0 ? word : word[0].toUpperCase() + word.slice(1))
+      .join('')
+  };
+
+  if (!convertor.hasOwnProperty(namingConvention)) {
+    resultElement.textContent = 'Error!';
+    return;
+  }
+
+  resultElement.textContent = convertor[namingConvention](text);
+  
 }
