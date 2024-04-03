5a1e024527e1ed0a481a9eb1140f1bba6b7b7641
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2024-04-03 11:40:35 +0200

    Initial Commit

--- 6.js_front_end_feb_2024/5.2.js_dom_introduction_exercise/04.search_in_list/search.js
+++ 6.js_front_end_feb_2024/5.2.js_dom_introduction_exercise/04.search_in_list/search.js
@@ -1,3 +1,16 @@
 function search() {
-   // TODO
+   const townListElement = document.getElementById('towns');
+   const searchTextElement = document.getElementById('searchText').value;
+   const resultElement = document.getElementById('result');
+   const townElements = Array.from(townListElement.children);
+   let matchCount = 0;
+
+   for (const townElement of townElements) {
+      if (townElement.textContent.toLowerCase().includes(searchTextElement.toLowerCase())) {
+         townElement.style.fontWeight = 'bold';
+         townElement.style.textDecoration = 'underline';
+         matchCount++;
+      }
+   }
+   resultElement.textContent = `${matchCount} matches found`;
 }
