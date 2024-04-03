dbc8cdab8dbae2a1496034e4f79d51cf62e52de3
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2024-04-03 11:23:44 +0200

    Initial Commit

--- 6.js_front_end_feb_2024/5.2.js_dom_introduction_exercise/03.accordion/accordion.js
+++ 6.js_front_end_feb_2024/5.2.js_dom_introduction_exercise/03.accordion/accordion.js
@@ -1,3 +1,13 @@
 function toggle() {
-    console.log('TODO:...');
+    const toggleButtonElement = document.querySelector('.head span.button');
+    const extraInformationElement = document.getElementById('extra');
+
+    const currentButtonText = toggleButtonElement.textContent;
+    if (currentButtonText === 'More') {
+        toggleButtonElement.textContent = 'Less';
+        extraInformationElement.style.display = 'block';
+    } else {
+        toggleButtonElement.textContent = 'More';
+        extraInformationElement.style.display = 'none';
+    }
 }
