283c6abf254dd512279973c623300701ef56bf98
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2024-04-03 12:11:55 +0200

    Initial Commit

--- 6.js_front_end_feb_2024/5.2.js_dom_introduction_exercise/05.table_search_engine/solution.js
+++ 6.js_front_end_feb_2024/5.2.js_dom_introduction_exercise/05.table_search_engine/solution.js
@@ -2,7 +2,25 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      const trElements = Array.from(document.querySelectorAll('table.container tbody tr'));
+      const searchFieldElement = document.querySelector('#searchField');
+      const searchText = searchFieldElement.value.toLowerCase();
+      
+      for (const trElement of trElements) {
+         const tdElements = trElement.querySelectorAll('td');
+         let isSelected = false;
+         trElement.classList.remove('select');
 
+         for (const tdElement of tdElements) {
+            if (tdElement.textContent.toLowerCase().includes(searchText)) {
+               isSelected = true;
+               break;
+            }
+         }
+         if (isSelected) {
+            trElement.classList.add('select');
+         }
+      }
+      searchFieldElement.value = '';
    }
 }
