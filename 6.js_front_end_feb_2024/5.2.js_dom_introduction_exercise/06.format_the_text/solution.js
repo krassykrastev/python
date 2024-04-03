047ba91b0cab89cd43cc4157fe6c91fdbf5adf68
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2024-04-03 12:38:35 +0200

    Initial Commit

--- 6.js_front_end_feb_2024/5.2.js_dom_introduction_exercise/06.format_the_text/solution.js
+++ 6.js_front_end_feb_2024/5.2.js_dom_introduction_exercise/06.format_the_text/solution.js
@@ -1,3 +1,23 @@
 function solve() {
-  //TODO
+  const outputElement = document.querySelector('#output');
+  const textAreaElement = document.querySelector('#input');
+  const text = textAreaElement.value;
+
+  const result = text
+    .split('.')
+    .filter(sentence => !!sentence)
+    .reduce((result, sentence, i) => {
+    const resultIndex = Math.floor(i / 3);
+    if (!result[resultIndex]) {
+      result[resultIndex] = [];
+    }
+
+    result[resultIndex].push(sentence.trim());
+
+    return result
+  }, [])
+    .map(sentences => `<p>${sentences.join('. ')}.</p>`)
+    .join('\n');
+
+  outputElement.innerHTML = result;
 }
