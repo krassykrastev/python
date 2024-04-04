8dcfa0fe1cf911367031256d7af18b3dace789da
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2024-04-04 14:32:15 +0200

    Initial Commit

--- 6.js_front_end_feb_2024/6.1.js_dom_events_lab/01.list_of_items/app.js
+++ 6.js_front_end_feb_2024/6.1.js_dom_events_lab/01.list_of_items/app.js
@@ -1,3 +1,16 @@
 function addItem() {
-    console.log('TODO:...');
+    const inputElement = document.getElementById('newItemText');
+    const itemListElement = document.getElementById('items');
+
+    //create new item
+    const newItemElement = document.createElement('li');
+
+    //add text content
+    newItemElement.textContent = inputElement.value;
+
+    //append to dom
+    itemListElement.appendChild(newItemElement);
+
+    //clear input element
+    inputElement.value = '';
 }
