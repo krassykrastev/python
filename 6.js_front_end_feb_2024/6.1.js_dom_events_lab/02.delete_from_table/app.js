d8b876a94d99667120ebd82369b838620666639d
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2024-04-04 14:48:19 +0200

    Initial Commit

--- 6.js_front_end_feb_2024/6.1.js_dom_events_lab/02.delete_from_table/app.js
+++ 6.js_front_end_feb_2024/6.1.js_dom_events_lab/02.delete_from_table/app.js
@@ -1,3 +1,25 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const inputEmailElement = document.querySelector('input[name=email]');
+    const resultElement = document.getElementById('result');
+    const trElements = document.querySelectorAll('table#customers tbody tr');
+
+    //search for tr element
+    const resultTrElement = Array
+        .from(trElements)
+        .find(trElement => trElement.children[1].textContent.includes(inputEmailElement.value));
+
+    if (resultTrElement) {
+    //delete table row
+    resultTrElement.remove();
+
+    //show deleted textContent
+    resultElement.textContent = 'Deleted.';
+
+    } else {
+    //show not found textContent
+    resultElement.textContent = 'Not found.';
+    }
+
+    //clear input
+    inputEmailElement.value = '';
 }
