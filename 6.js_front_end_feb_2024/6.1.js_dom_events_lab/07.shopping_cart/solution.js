ef4d8c240debcc69a626ca5f283ec2bc2b59ed29
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2024-04-05 07:29:52 +0200

    Initial Commit

--- 6.js_front_end_feb_2024/6.1.js_dom_events_lab/07.shopping_cart/solution.js
+++ 6.js_front_end_feb_2024/6.1.js_dom_events_lab/07.shopping_cart/solution.js
@@ -1,3 +1,27 @@
 function solve() {
-   //TODO...
+   const addProductButtonsElements = document.querySelectorAll('button.add-product');
+   const textAreaElement = document.querySelector('textarea');
+   const checkOutButtonElement = document.querySelector('button.checkout');
+   let totalPrice = 0;
+   let uniqueProducts = {};
+   
+   for (const buttonElement of addProductButtonsElements) {
+      const productElement = buttonElement.parentElement.parentElement;
+
+      buttonElement.addEventListener('click', () => {
+         const title = productElement.querySelector('.product-title').textContent;
+         const price = Number(productElement.querySelector('.product-line-price').textContent);
+         totalPrice += price;
+         uniqueProducts[title] = true;
+
+         textAreaElement.textContent += `Added ${title} for ${price.toFixed(2)} to the cart.\n`;
+      });
+   }
+   checkOutButtonElement.addEventListener('click', (event) => {
+      Array.from(addProductButtonsElements).forEach(buttonElement => buttonElement.disabled = true);
+      event.target.disabled = true;
+      const list = Object.keys(uniqueProducts).join(', ');
+
+      textAreaElement.textContent += `You bought ${list} for ${totalPrice.toFixed(2)}.`;
+   });
 }
