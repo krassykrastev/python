5eb055d2097ea1cb71f1d3cb58b1627df1860865
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2024-04-05 07:03:27 +0200

    Initial Commit

--- 6.js_front_end_feb_2024/6.1.js_dom_events_lab/06.dynamic_validation/app.js
+++ 6.js_front_end_feb_2024/6.1.js_dom_events_lab/06.dynamic_validation/app.js
@@ -1,3 +1,13 @@
 function validate() {
-    console.log('TODO:...');
+    const emailInputElement = document.getElementById('email');
+    const pattern = /^[a-z]+@[a-z]+\.[a-z]+$/;
+
+    emailInputElement.addEventListener('change', (event) => {
+        const email = emailInputElement.value;
+        if (pattern.test(email)) {
+            emailInputElement.classList.remove('error');
+        } else {
+            emailInputElement.classList.add('error');
+        }    
+    });       
 }
