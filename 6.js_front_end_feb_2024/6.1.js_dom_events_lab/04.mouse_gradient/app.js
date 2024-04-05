e13388c3d046bcfd45b523890c3ea1a1203f577f
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2024-04-05 05:52:40 +0200

    Initial Commit

--- 6.js_front_end_feb_2024/6.1.js_dom_events_lab/04.mouse_gradient/app.js
+++ 6.js_front_end_feb_2024/6.1.js_dom_events_lab/04.mouse_gradient/app.js
@@ -1,3 +1,9 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    const gradientElement = document.getElementById('gradient');
+    const resultElement = document.getElementById('result');
+
+    gradientElement.addEventListener('mousemove', (event) => {
+        const percent = Math.floor(event.offsetX / event.target.clientWidth * 100);
+        resultElement.textContent = `${percent}%`;
+    });
 }
