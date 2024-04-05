fe51cb26efab9b258e39d1f9669dd4f303342576
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2024-04-05 05:33:05 +0200

    Initial Commit

--- 6.js_front_end_feb_2024/6.1.js_dom_events_lab/03.add_delete/app.js
+++ 6.js_front_end_feb_2024/6.1.js_dom_events_lab/03.add_delete/app.js
@@ -1,3 +1,28 @@
 function addItem() {
-    //TODO...
+    const inputItemElement = document.getElementById('newItemText');
+    const itemsListElement = document.getElementById('items');
+
+    //create new item element
+    const newInputElement = document.createElement('li');
+    newInputElement.textContent = inputItemElement.value;
+
+    //create detele link element
+    const deleteLinkElement = document.createElement('a');
+    deleteLinkElement.textContent = '[Delete]';
+    deleteLinkElement.href = '#';
+
+    //add event to delete link element
+    deleteLinkElement.addEventListener('click', () => {
+        newInputElement.remove();
+    });
+
+    //append delete link element to new item element
+    newInputElement.appendChild(deleteLinkElement);
+
+    //append new item element to dom
+    itemsListElement.appendChild(newInputElement);
+
+    //clear input
+    inputItemElement.value = '';
+
 }
