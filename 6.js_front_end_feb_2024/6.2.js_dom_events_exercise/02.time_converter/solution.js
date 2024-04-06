d8bd13d52fbb41eb703bb4026b51b1bb6f2479b3
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2024-04-06 12:12:55 +0200

    Initial Commit

--- 6.js_front_end_feb_2024/6.2.js_dom_events_exercise/02.time_converter/solution.js
+++ 6.js_front_end_feb_2024/6.2.js_dom_events_exercise/02.time_converter/solution.js
@@ -1,4 +1,45 @@
 function attachEventsListeners() {
+    const convertButtonElements = document.querySelectorAll('input[type=button][value=Convert]');
+    const inputElements = document.querySelectorAll('input[type=text]');
 
-    console.log('TODO:...');
+    const toSeconds = (value, unit) => {
+        switch (unit) {
+            case 'days':
+                return value * 24 * 60 * 60;
+            case 'hours':
+                return value * 60 * 60;
+            case 'minutes':
+                return value * 60;
+            case 'seconds':
+                return value;
+        }
+    }
+
+    const converters = {
+        days(seconds) {
+            return seconds / (24 * 60 * 60);
+        },
+        hours(seconds) {
+            return seconds / (60 * 60);
+        },
+        minutes(seconds) {
+            return seconds / 60;
+        },
+        seconds(seconds) {
+            return seconds;
+        }
+    }
+
+    for (const buttonElement of convertButtonElements) {
+        buttonElement.addEventListener('click', (e) => {
+            const currentInputElement = e.currentTarget.previousElementSibling;
+        
+            for (const inputElement of inputElements) {
+                const seconds = toSeconds(Number(currentInputElement.value), currentInputElement.id);
+                inputElement.value = converters[inputElement.id](seconds);
+            }
+
+
+        });
+    }
 }
