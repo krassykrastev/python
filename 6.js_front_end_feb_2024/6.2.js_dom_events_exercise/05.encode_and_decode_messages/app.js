656fbea5bccce700f2273b6b64e0b72691655af8
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2024-04-06 14:18:04 +0200

    Initial Commit

--- 6.js_front_end_feb_2024/6.2.js_dom_events_exercise/05.encode_and_decode_messages/app.js
+++ 6.js_front_end_feb_2024/6.2.js_dom_events_exercise/05.encode_and_decode_messages/app.js
@@ -1,3 +1,36 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const firstTextBoxElement = document.querySelector('#main div:nth-child(1) textarea');
+    const firstTextButtonElement = document.querySelector('#main div:nth-child(1) button');
+
+    const secondTextBoxElement = document.querySelector('#main div:nth-child(2) textarea');
+    const secondTextButtonElement = document.querySelector('#main div:nth-child(2) button');
+
+    firstTextButtonElement.addEventListener('click', () => {
+        const originalText = firstTextBoxElement.value;
+        firstTextBoxElement.value = '';
+        secondTextBoxElement.value = shiftString(originalText, true);
+    })
+
+    secondTextButtonElement.addEventListener('click', () => {
+        const originalText = secondTextBoxElement.value;
+        secondTextBoxElement.value = '';
+        secondTextBoxElement.value = shiftString(originalText, false);
+    })
+
+    function shiftString(str, up) {
+        let shiftedString = '';
+        for (let i = 0; i < str.length; i++) {
+          const currentCharCode = str.charCodeAt(i);
+          if (up) {
+            const newCharCode = currentCharCode + 1;
+            shiftedString += String.fromCharCode(newCharCode);
+          } else {
+            const newCharCode = currentCharCode - 1;
+            shiftedString += String.fromCharCode(newCharCode);
+          }
+          
+        }
+        return shiftedString;
+      }
+
 }
