0cb47b9b1acc158fe283c306f6a95be6418bc177
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2024-04-06 14:13:28 +0200

    Initial Commit

--- 6.js_front_end_feb_2024/6.2.js_dom_events_exercise/04.fill_dropdown/dropdown.js
+++ 6.js_front_end_feb_2024/6.2.js_dom_events_exercise/04.fill_dropdown/dropdown.js
@@ -1,3 +1,14 @@
 function addItem() {
-    console.log('TODO:...');
+    const inputTextElement = document.getElementById('newItemText');
+    const inputValueElement = document.getElementById('newItemValue');
+    const selectElement = document.getElementById('menu');
+
+    const optionElement = document.createElement('option');
+    optionElement.textContent = inputTextElement.value;
+    optionElement.value = inputValueElement.value;
+
+    selectElement.appendChild(optionElement);
+
+    inputTextElement.value = '';
+    inputValueElement.value = '';
 }
