20089fec08a05f97c33c07df7075e6fac1d174f6
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2024-04-06 12:38:09 +0200

    Initial Commit

--- 6.js_front_end_feb_2024/6.2.js_dom_events_exercise/03.locked_profile/app.js
+++ 6.js_front_end_feb_2024/6.2.js_dom_events_exercise/03.locked_profile/app.js
@@ -1,3 +1,23 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const profileElements = document.querySelectorAll('.profile');
+    for (const profileElement of profileElements) {
+        const showButtonElement = profileElement.querySelector('button');
+        const lockRadioElement = profileElement.querySelector('input[type=radio][value=lock]');
+
+        showButtonElement.addEventListener('click', (e) => {
+            if (lockRadioElement.checked) {
+                return;
+            }
+
+            const additionInfoElement = showButtonElement.previousElementSibling;
+
+            if (showButtonElement.textContent === 'Show more') {
+                additionInfoElement.style.display = 'block';
+                showButtonElement.textContent = "Hide it";
+            } else {
+                additionInfoElement.style.display = 'none';
+                showButtonElement.textContent = "Show more";
+            }
+        });
+    }
 }
