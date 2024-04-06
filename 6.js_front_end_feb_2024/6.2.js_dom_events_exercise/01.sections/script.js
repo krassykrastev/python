bc72055f7bfcf5975d4119971eb47cd2a359b513
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2024-04-06 11:00:04 +0200

    Initial Commit

--- 6.js_front_end_feb_2024/6.2.js_dom_events_exercise/01.sections/script.js
+++ 6.js_front_end_feb_2024/6.2.js_dom_events_exercise/01.sections/script.js
@@ -1,3 +1,34 @@
 function create(words) {
-   console.log('TODO:...');
+   const contentElement = document.getElementById('content');
+   
+   //create div with paragraph
+   const divElements = words
+      .map(word => {
+         //create paragraph
+         const pElement = document.createElement('p');
+         
+         //add text content
+         pElement.textContent = word;
+         
+         //hide the paragraph
+         pElement.style.display = 'none';
+
+         //create div
+         const divElement = document.createElement('div');
+
+         //append paragraph to div
+         divElement.appendChild(pElement);
+
+         //show paragraph on div click
+         divElement.addEventListener('click', () => {
+            pElement.style.display = 'block';
+         });
+
+         //return div
+         return divElement;
+      });
+
+
+   //append all to dom
+   divElements.forEach(divElement => contentElement.appendChild(divElement));
 }
