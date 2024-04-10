dbf5e290ddad2f9bcdd395eb3e365e630f7b62d4
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2024-04-10 11:23:52 +0200

    Initial Commit

--- 6.js_front_end_feb_2024/8.1.js_asynchronous_programming_lab/01.bus_stop/app.js
+++ 6.js_front_end_feb_2024/8.1.js_asynchronous_programming_lab/01.bus_stop/app.js
@@ -1,3 +1,23 @@
 function getInfo() {
-    // TODO:
+    const baseUrl = 'http://localhost:3031/jsonstore/bus/businfo';
+    
+    const stopIdElement = document.getElementById('stopId');
+    const stopNameElement = document.getElementById('stopName');
+    const busesElement = document.getElementById('buses');
+
+    const stopId = stopIdElement.value;
+    fetch(`${baseUrl}/${stopId}`)
+        .then(response => response.json())
+        .then(data => {
+            stopNameElement.textContent = data.name;
+
+            for (const busId in data.buses) {
+                const liElement = document.createElement('li');
+                liElement.textContent = `Bus ${busId} arrives in ${data.buses[busId]}`;
+                busesElement.appendChild(liElement);
+                }
+            }
+    )
+        .catch(() => stopNameElement.textContent = 'Error');
+    ;
 }
--- 6.js_front_end_feb_2024/8.1.js_asynchronous_programming_lab/server/server.js
+++ 6.js_front_end_feb_2024/8.1.js_asynchronous_programming_lab/server/server.js
@@ -1366,7 +1366,7 @@
 
     const server = http__default['default'].createServer(requestHandler(plugins, services));
 
-    const port = 3030;
+    const port = 3031;
     server.listen(port);
     console.log(`Server started on port ${port}. You can make requests to http://localhost:${port}/`);
     console.log(`Admin panel located at http://localhost:${port}/admin`);
